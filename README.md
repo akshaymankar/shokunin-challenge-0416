3d6ceee8dc192e22ebed911664a04bf924df11a0
Akshay Mankar <itsakshaymankar@gmail.com>
2016-04-13 23:03:45 +1000

    Add readme, license and go.sh

---
+++ README.md
@@ -0,0 +1,19 @@
+# Shokunin Challenge April 2016
+
+## Magic Square
+
+### What does it do?
+
+It generates a magic square of size n (for all n > 0)
+
+### What is a magic square?
+
+Read here: https://en.wikipedia.org/wiki/Magic_square
+
+### How do I run it?
+
+Run this command:
+
+    $ ./go.sh <n>
+
+Where n is the number of rows (and columns (duh!)) in the square.
---
+++ go.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Can't build magic squares, you'd have to do with this:"
+echo "╔═════════════════════════╗"
+echo "║                         ║"
+echo "║                         ║"
+echo "║                         ║"
+echo "║                         ║"
+echo "║                         ║"
+echo "║                         ║"
+echo "║                         ║"
+echo "║                         ║"
+echo "║                         ║"
+echo "║                         ║"
+echo "╚═════════════════════════╝"
+
+echo "I tried my best."
+exit 1
